5b75ab3dd5e031872d77a33a15cb2c9a19382e87
MANDALORE\nilsg <test@gmail.com>
2025-12-29 14:26:44 +0100

    feat: remove model testing programm for esp32s3

--- hardware/firmware/esp32cam/TestBumblebeeModel/main/include/ClassificationPostProcessor.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "dl_cls_postprocessor.hpp"
-#include "dl_model_base.hpp"
-
-class ClassificationPostProcessor : public dl::cls::ClsPostprocessor {
-public:
-    ClassificationPostProcessor(dl::Model *model, const int top_k, const float score_thr, bool need_softmax, const std::string &output_name = "");
-};
--- hardware/firmware/esp32cam/TestBumblebeeModel/main/include/classification_category_name.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-// Nur Hintergrund und Hummel
-static const char *classification_cat_names[] = {
-    "background",
-    "bumblebee"
-};
